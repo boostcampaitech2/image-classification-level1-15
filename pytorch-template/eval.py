29d3537ab77af218a69dea16aba0bc3f32217346
keemdy <keemdy311@gmail.com>
2021-08-27 14:08:32 +0900

    ViT test

---
+++ pytorch-template/eval.py
@@ -0,0 +1,84 @@
+import argparse
+import torch
+from tqdm import tqdm
+import data_loader.data_loaders as module_data
+import model.loss as module_loss
+import model.metric as module_metric
+import model.model as module_arch
+from parse_config import ConfigParser
+import pandas as pd
+
+
+def main(config):
+    logger = config.get_logger('test')
+
+    # setup data_loader instances
+    data_loader = getattr(module_data, config['data_loader']['type'])(
+        config['data_loader']['args']['data_dir'],
+        batch_size=32,
+        shuffle=False,
+        validation_split=0.0,
+        training=False,
+        num_workers=4,
+        csv_path=config['data_loader']['args']['csv_path'],
+    )
+
+    print()
+
+    # build model architecture
+    model = config.init_obj('arch', module_arch)
+    logger.info(model)
+
+    # get function handles of loss and metrics
+    loss_fn = getattr(module_loss, config['loss'])
+    metric_fns = [getattr(module_metric, met) for met in config['metrics']]
+
+    logger.info('Loading checkpoint: {} ...'.format(config.resume))
+    checkpoint = torch.load(config.resume)
+    state_dict = checkpoint['state_dict']
+    if config['n_gpu'] > 1:
+        model = torch.nn.DataParallel(model)
+    model.load_state_dict(state_dict)
+
+    # prepare model for testing
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    model.eval()
+
+    total_loss = 0.0
+    total_metrics = torch.zeros(len(metric_fns))
+
+    submission = pd.read_csv(
+        config['data_loader']['args']['data_dir'] + 'eval/info.csv')
+
+    all_predictions = []
+    with torch.no_grad():
+        for i, image in enumerate(tqdm(data_loader)):
+            image = image.to(device)
+
+            output = model(image)
+            pred = output.argmax(dim=-1)
+            all_predictions.extend(pred.cpu().numpy())
+
+    submission['ans'] = all_predictions
+    submission.to_csv('submission.csv', index=False)
+
+    n_samples = len(data_loader.sampler)
+    log = {'loss': total_loss / n_samples}
+    log.update({
+        met.__name__: total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)
+    })
+    logger.info(log)
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='PyTorch Template')
+    args.add_argument('-c', '--config', default=None, type=str,
+                      help='config file path (default: None)')
+    args.add_argument('-r', '--resume', default=None, type=str,
+                      help='path to latest checkpoint (default: None)')
+    args.add_argument('-d', '--device', default=None, type=str,
+                      help='indices of GPUs to enable (default: all)')
+
+    config = ConfigParser.from_args(args)
+    main(config)
---
+++ pytorch-template/model/loss.py
@@ -0,0 +1,9 @@
+import torch.nn.functional as F
+
+
+def nll_loss(output, target):
+    return F.nll_loss(output, target)
+
+
+def cross_entropy_loss(output, target):
+    return F.cross_entropy(output, target)
